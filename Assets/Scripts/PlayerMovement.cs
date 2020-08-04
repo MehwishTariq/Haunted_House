d4f48e12b2d5f991b9f8bfbd8f2199e3d4071308
MEHWISH TARIQ <mickeyminnie92@hotmail.com>
2020-08-04 21:19:35 +0500

    Added files

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Animator animator;
+    Rigidbody m_Rigidbody;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+    public AudioSource footsteps;
+
+    public float turnSpeed = 20f;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+        footsteps = GetComponent<AudioSource>();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        animator.SetBool("IsWalking", isWalking);
+
+        if (isWalking)
+        {
+            if (!footsteps.isPlaying)
+            {
+                footsteps.Play();
+            }
+        }
+        else
+        {
+            footsteps.Stop();
+        }
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
+        
+    }
+
+}
